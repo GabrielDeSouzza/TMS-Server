ae6e70ec019058966c43da55dd1c351214d5c23d
Gabriel de Souza <Gabrielemail4321@gmail.com>
2023-07-16 20:13:35 -0300

    feat: add entity contract outsourced driver

---
+++ src/domain/entities/personEntities/contractOutsourcedDriver/ContractOutsourcedDriver.spec.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { OutsourcedVehicle } from '../../vehicleEntities/outsourcedVehicle/OutsourcedVehicle';
+import { Vehicle } from '../../vehicleEntities/vehicle/Vehicle';
+import { VehicleBrand } from '../../vehicleEntities/vehicleBrand/VehicleBrand';
+import { VehicleModel } from '../../vehicleEntities/vehicleModel/VehicleModel';
+import { NaturalPerson } from '../naturalPerson/NaturalPerson';
+import { OutsourcedDriver } from '../outsourcedDriver/OutsourcedDriver';
+import { ContractOutsourcedDriver } from './ContractOutsourcedDriver';
+
+describe('Contract Outsourced Driver', () => {
+  it('should create ContractOutsourcedDriver', () => {
+    const contractOutsourcedDriver = new ContractOutsourcedDriver({
+      cpf: '12345678910',
+      situation: 'Em aberto',
+      start_at: new Date(),
+      end_at: null,
+      type: 'Eteasd',
+      OutsourcedDriver: new OutsourcedDriver({
+        NaturalPerson: new NaturalPerson({
+          name: 'Jão',
+          date_birth: new Date(),
+          gender: 'Masculino',
+          cpf: '75154789711',
+          rg: '1475547854',
+          cep: 12_345_678,
+          public_place: 'Rua Principal',
+          address_number: '123',
+          neighborhood: 'Centro',
+          complement: 'Apartamento 456',
+          city: 'São Paulo',
+          uf: 'SP',
+          first_phone: '155455',
+          second_phone: null,
+          third_phone: '333333333',
+          email: 'joao@example.com',
+          nationality: 'Brasileiro',
+        }),
+        cnh: '48848878781',
+        cnh_category: 'E',
+        cnh_expiration: 1125,
+        company_vehicle: false,
+        course_mopp: true,
+        OutsourcedVehicle: new OutsourcedVehicle({
+          Vehicle: new Vehicle({
+            plate: '455445',
+            color: 'sdf',
+            renavam: '449984944',
+            rntrc_expiration: 'dsa',
+            year: '455',
+            VehicleModel: new VehicleModel({
+              axles: 4,
+              capacity_max: 40_000,
+              name: 'Unão',
+              weight: 4000,
+              VehicleBrand: new VehicleBrand({
+                name: 'Ford',
+              }),
+            }),
+          }),
+        }),
+      }),
+    });
+
+    expect(contractOutsourcedDriver).toBeTruthy();
+  });
+});
---
+++ src/domain/entities/personEntities/contractOutsourcedDriver/ContractOutsourcedDriver.ts
@@ -0,0 +1,142 @@
+import { randomUUID } from 'node:crypto';
+
+import { type Replace } from 'helpers/Replace';
+
+import { Entity } from '../../../shared/entities/Entity';
+import { type IValidationField } from '../../../shared/notification/Notification';
+import { NotificationError } from '../../../shared/notification/NotificationError';
+import { type OutsourcedDriver } from '../outsourcedDriver/OutsourcedDriver';
+
+interface IContractOutsourcedDriver {
+  type: string;
+  situation: string;
+  start_at: Date;
+  end_at?: Date;
+  created_at: Date;
+  updated_at: Date;
+  cpf: string;
+  OutsourcedDriver: OutsourcedDriver;
+}
+export class ContractOutsourcedDriver extends Entity {
+  private _id: string;
+  private props: IContractOutsourcedDriver;
+
+  constructor(
+    props: Replace<
+      IContractOutsourcedDriver,
+      { created_at?: Date; updated_at?: Date }
+    >,
+    id?: string,
+  ) {
+    super();
+
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      updated_at: new Date(),
+      created_at: props.created_at ?? new Date(),
+    };
+    this.validate();
+
+    if (this.notification.hasErrors()) {
+      const errors = this.notification.getErrors();
+
+      throw new NotificationError(errors);
+    }
+  }
+
+  validate() {
+    const fieldsValidation: Array<IValidationField> =
+      new Array<IValidationField>();
+    fieldsValidation.push(
+      {
+        field: this.cpf,
+        fieldName: 'CPF',
+        maxLength: 11,
+        minLength: 11,
+      },
+      {
+        field: this.end_at,
+        fieldName: 'End at',
+        maxLength: 15,
+        isNullAble: true,
+      },
+      {
+        field: this.situation,
+        fieldName: 'Situation',
+        maxLength: 50,
+      },
+      { field: this.start_at, fieldName: 'Start_at', maxLength: 15 },
+      { field: this.type, fieldName: 'Type', maxLength: 50 },
+    );
+    this.notification.requiredField(
+      'ContractOutsourcedDriver',
+      fieldsValidation,
+    );
+    this.OutsourcedDriver.validate();
+  }
+
+  public set type(type: string) {
+    this.props.type = type;
+  }
+
+  public get type(): string {
+    return this.props.type;
+  }
+
+  public set situation(situation: string) {
+    this.props.situation = situation;
+  }
+
+  public get situation(): string {
+    return this.props.situation;
+  }
+
+  public set start_at(startAt: Date) {
+    this.props.start_at = startAt;
+  }
+
+  public get start_at(): Date {
+    return this.props.start_at;
+  }
+
+  public set end_at(endAt: Date | undefined) {
+    this.props.end_at = endAt;
+  }
+
+  public get end_at(): Date | undefined {
+    return this.props.end_at;
+  }
+
+  public set created_at(createdAt: Date) {
+    this.props.created_at = createdAt;
+  }
+
+  public get created_at(): Date {
+    return this.props.created_at;
+  }
+
+  public set updated_at(updatedAt: Date) {
+    this.props.updated_at = updatedAt;
+  }
+
+  public get updated_at(): Date {
+    return this.props.updated_at;
+  }
+
+  public set cpf(cpf: string) {
+    this.props.cpf = cpf;
+  }
+
+  public get cpf(): string {
+    return this.props.cpf;
+  }
+
+  public set OutsourcedDriver(outsourcedDriver: OutsourcedDriver) {
+    this.props.OutsourcedDriver = outsourcedDriver;
+  }
+
+  public get OutsourcedDriver(): OutsourcedDriver {
+    return this.props.OutsourcedDriver;
+  }
+}
