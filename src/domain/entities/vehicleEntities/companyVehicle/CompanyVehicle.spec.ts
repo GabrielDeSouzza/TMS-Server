4210c523c37445d1f575b26c6fcaaa0f00804a6a
Gabriel de Souza <Gabrielemail4321@gmail.com>
2023-07-17 20:55:55 -0300

    feat: add entity company vehicle

---
+++ src/domain/entities/vehicleEntities/companyVehicle/CompanyVehicle.spec.ts
@@ -0,0 +1,29 @@
+import { Vehicle } from '../vehicle/Vehicle';
+import { VehicleBrand } from '../vehicleBrand/VehicleBrand';
+import { VehicleModel } from '../vehicleModel/VehicleModel';
+import { CompanyVehicle } from './CompanyVehicle';
+
+describe('OutsourcedVehicle', () => {
+  it('should create outsourced Vehicle', () => {
+    const outsourcedVehicle = new CompanyVehicle({
+      Vehicle: new Vehicle({
+        plate: '455445',
+        color: 'Azul',
+        renavam: '449984944',
+        rntrc_expiration: 'dsa',
+        year: '455',
+        VehicleModel: new VehicleModel({
+          axles: 4,
+          capacity_max: 40_000,
+          name: 'Unão',
+          weight: 4000,
+          VehicleBrand: new VehicleBrand({
+            name: 'Ford',
+          }),
+        }),
+      }),
+    });
+
+    expect(outsourcedVehicle).toBeTruthy();
+  });
+});
---
+++ src/domain/entities/vehicleEntities/companyVehicle/CompanyVehicle.ts
@@ -0,0 +1,69 @@
+import { randomUUID } from 'node:crypto';
+
+import { type Replace } from 'helpers/Replace';
+
+import { Entity } from '../../../shared/entities/Entity';
+import { NotificationError } from '../../../shared/notification/NotificationError';
+import { type Vehicle } from '../vehicle/Vehicle';
+
+interface ICompanyVehicle {
+  Vehicle: Vehicle;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export class CompanyVehicle extends Entity {
+  private _id: string;
+  private props: ICompanyVehicle;
+
+  constructor(
+    props: Replace<ICompanyVehicle, { created_at?: Date; updated_at?: Date }>,
+    id?: string,
+  ) {
+    super();
+
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      updated_at: new Date(),
+      created_at: props.created_at ?? new Date(),
+    };
+    this.validate();
+
+    if (this.notification.hasErrors()) {
+      const errors = this.notification.getErrors();
+
+      throw new NotificationError(errors);
+    }
+  }
+
+  validate() {
+    this.props.Vehicle.validate();
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get Vehicle(): Vehicle {
+    return this.props.Vehicle;
+  }
+  public set Vehicle(vehicle: Vehicle) {
+    this.props.Vehicle = vehicle;
+  }
+  public set created_at(createdAt: Date) {
+    this.props.created_at = createdAt;
+  }
+
+  public get created_at(): Date {
+    return this.props.created_at;
+  }
+
+  public set updated_at(updatedAt: Date) {
+    this.props.updated_at = updatedAt;
+  }
+
+  public get updated_at(): Date {
+    return this.props.updated_at;
+  }
+}
