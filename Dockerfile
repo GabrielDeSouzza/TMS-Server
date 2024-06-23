94d64ce09bf2b4df03fb8fc5f802e775d3a3ea4f
Gabriel Guedes <gabrielrguedess@gmail.com>
2024-06-23 18:11:54 -0300

    feat: add new pdf generator

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,15 @@ RUN pnpm fetch --prod
 
 COPY --chown=node:node . .
 
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
+RUN apt-get update && apt-get install gnupg wget -y && \
+  wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
+  sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+  apt-get update && \
+  apt-get install google-chrome-stable -y --no-install-recommends && \
+  rm -rf /var/lib/apt/lists/*
+
 RUN pnpm install
 
 RUN pnpm prisma generate
@@ -29,6 +38,15 @@ COPY --chown=node:node pnpm-lock.yaml ./
 COPY --chown=node:node --from=development /usr/src/app/node_modules ./node_modules
 
 COPY --chown=node:node . .
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
+RUN apt-get update && apt-get install gnupg wget -y && \
+  wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
+  sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+  apt-get update && \
+  apt-get install google-chrome-stable -y --no-install-recommends && \
+  rm -rf /var/lib/apt/lists/*
 
 ENV NODE_ENV production
 
--- src/infra/services/generatePdf/GeneratePdf.ts
+++ src/infra/services/generatePdf/GeneratePdf.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-import { GraphQLError } from 'graphql';
-import { type CreateOptions } from 'html-pdf';
-import pdf from 'html-pdf';
+import puppeteer from 'puppeteer';
 
 import { UploaderProvider } from 'domain/providers/UploaderProvider';
 
@@ -11,28 +9,22 @@ export class GeneratePdfService {
   constructor(private cloudFilesService: UploaderProvider) {}
 
   async generatePdf(html: string, fileName: string): Promise<string> {
-    const options: CreateOptions = {
-      format: 'A4',
-      type: 'pdf',
-    };
-
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      pdf.create(html, options).toBuffer(async (error, buffer) => {
-        if (error) {
-          reject(new GraphQLError('Erro ao Gerar PDF'));
-        } else {
-          try {
-            const url = await this.cloudFilesService.uploadPdf(
-              buffer,
-              fileName,
-            );
-            resolve(url.path);
-          } catch (uploadError) {
-            reject(uploadError);
-          }
-        }
-      });
+    const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/google-chrome',
     });
+    const page = await browser.newPage();
+    await page.setContent(html);
+    const pdf = await page.pdf({ path: fileName, format: 'A4' });
+    await browser.close();
+
+    try {
+      const url = await this.cloudFilesService.uploadPdf(pdf, fileName);
+
+      console.log(url);
+
+      return url.path;
+    } catch (uploadError) {
+      console.error(uploadError);
+    }
   }
 }
